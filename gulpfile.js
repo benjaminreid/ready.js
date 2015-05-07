2ed8d21cfc504aa6708c6651dc39f82af0c3898a
Benjamin Reid <benjamin@nouveller.com>
2015-05-07 22:24:21 +0100

    Create build of ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,18 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('copy', function() {
+  gulp.src('./src/ready.js')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('minify', ['copy'], function() {
+  gulp.src('./dist/ready.js')
+    .pipe(rename('ready.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'));
+});
 
 gulp.task('default', function() {
   console.log('Hello from ready.js');
