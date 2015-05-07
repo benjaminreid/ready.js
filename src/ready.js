f08136125ae12be62ae4fd3ae5f675f1e48f24bd
Benjamin Reid <benjaminreid.nouveller@gmail.com>
2015-05-07 12:51:20 +0100

    Support multiple elements

--- src/ready.js
+++ src/ready.js
@@ -4,13 +4,15 @@
   var ready;
 
   ready = function(selector, callback) {
-    // get the element that the callback depends on
-    var element = document.querySelector(selector);
+    // get a list of elements that the callback depends on
+    var elements = document.querySelectorAll(selector);
 
-    // if we have the element we fire the callback and pass the element
+    // if we have the elements we fire the callbacks and pass the element
     // as an argument
-    if (element) {
-      callback(element);
+    if (elements.length) {
+      for(var i = 0; i < elements.length; i++) {
+        callback(elements[i]);
+      }
     }
   };
 
