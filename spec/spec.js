31dab7f593fa42d402babecb474ae19ba9b597da
Benjamin Reid <benjaminreid.nouveller@gmail.com>
2015-05-14 11:51:08 +0100

    Add first proper test

--- spec/spec.js
+++ spec/spec.js
@@ -1,5 +1,13 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+describe('ready.js', function() {
+
+  it('runs your code when a dom element is in the DOM', function() {
+    var output = 'bar';
+    var selector = '.ready';
+
+    ready(selector, function(el) {
+      el.innerHTML = output;
+    });
+
+    expect(document.querySelector(selector).innerHTML).toBe(output);
   });
 });
