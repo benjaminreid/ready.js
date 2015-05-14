036db40be212420271ab98211a308e07a6ff6c40
Benjamin Reid <benjaminreid.nouveller@gmail.com>
2015-05-14 11:30:32 +0100

    Setup jasmine specs

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var webserver = require('gulp-webserver');
 require('gulp-release-tasks')(gulp);
+
+var jasmine_files = [
+  './bower_components/jasmine-core/lib/jasmine-core/jasmine.css',
+  './bower_components/jasmine-core/lib/jasmine-core/jasmine.js',
+  './bower_components/jasmine-core/lib/jasmine-core/jasmine-html.js',
+  './bower_components/jasmine-core/lib/jasmine-core/boot.js',
+];
 
 gulp.task('copy', function() {
   gulp.src('./src/ready.js')
@@ -13,6 +21,18 @@ gulp.task('minify', ['copy'], function() {
     .pipe(rename('ready.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('prepare-spec', function() {
+  gulp.src(jasmine_files)
+    .pipe(gulp.dest('./spec/lib'));
+});
+
+gulp.task('spec', ['prepare-spec'], function() {
+  gulp.src('./spec')
+    .pipe(webserver({
+      open: true
+    }));
 });
 
 gulp.task('default', function() {
